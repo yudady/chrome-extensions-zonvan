4b84c21d3a82f48a8edfd1f6f604d82b39fd5c01
yu_dady@yahoo.com.tw <yu650715>
2018-10-01 14:50:57 +0800

    1

--- popup.js
+++ popup.js
@@ -2,7 +2,8 @@ chrome.tabs.getSelected(null, (tab) => {
     var tabId = tab.id;
     var sendUserNamePassword = (fun) => chrome.tabs.executeScript(tabId, fun);
 
-    // 
+    // 抓取 popup.html的Dom 加入 click listener ，执行 `chrome.tabs.executeScript(tabId, fun)` fun是当前打开页面的js
+    // 下面是设定 账号 密码 验证码 然后点击btn，有汇入jquery所以可以直接用jquery抓取元素
     document.getElementById('myPay').addEventListener('click', () => {
         "use strict";
         sendUserNamePassword({
@@ -27,10 +28,13 @@ chrome.tabs.getSelected(null, (tab) => {
             code: 'document.getElementById("name").value = "zonvan";document.getElementById("pwd").value = "Zonvan123";document.getElementById("AccountDoLoginBtn").click(); '
         })
     });
-    document.getElementById('myCenterTest').addEventListener('click', () => {
-        "use strict";
-        sendUserNamePassword({
+
+
+    // 使用jquery
+    $("#myCenterTest").on('click', function () {
+        chrome.tabs.executeScript(tabId, {
             code: 'document.getElementById("name").value = "mycenter";document.getElementById("pwd").value = "Mycenter123";document.getElementById("code").value = "c";document.getElementById("AccountDoLoginBtn").click(); '
-        })
-    });
+        });
+
+    })
 });
